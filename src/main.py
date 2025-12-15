13aa3c4ad8469ab0abf5126e94d981aa510ff68c
EnderEzhik <Achernov2007@gmail.com>
2025-12-15 23:11:02 +0300

    Добавлено логирование

--- src/main.py
+++ src/main.py
@@ -3,9 +3,13 @@ from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
 
 from src.api.routes import auth, chat
+from src.core.logging import setup_logging
+
 
 
 app = FastAPI()
+
+setup_logging()
 
 app.mount("/static", StaticFiles(directory="src/static"), name="static")
 
